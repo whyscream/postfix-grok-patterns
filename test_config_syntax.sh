a0c98f7fbf64903055271fac1493a9b7e48b61e9
Tom Hendrikx <tom@whyscream.net>
2024-07-05 01:51:56 +0200

    Use latest logstash

--- test_config_syntax.sh
+++ test_config_syntax.sh
@@ -11,5 +11,5 @@ set -eux
 docker run --rm -it \
   --volume "$(pwd)"/postfix.grok:/etc/logstash/patterns.d/postfix.grok \
   --volume "$(pwd)"/50-filter-postfix.conf:/usr/share/logstash/pipeline/50-filter-postfix.conf \
-  logstash:8.12.0 \
+  logstash:8.14.1 \
   logstash --config.test_and_exit -f /usr/share/logstash/pipeline/50-filter-postfix.conf
